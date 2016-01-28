54f1ec763b04894424152830046248c1c7fbaccd
manman <manman@10.18.151.189>
2016-01-28 09:59:45 -0700

    Jan,28 I worked on that from Taylor

--- ultimateChess/src/byui/cit260/ultimateChess/model/Player.java
+++ ultimateChess/src/byui/cit260/ultimateChess/model/Player.java
@@ -5,10 +5,64 @@
  */
 package byui.cit260.ultimateChess.model;
 
+import java.io.Serializable;
+import java.util.Objects;
 /**
  *
  * @author Tanman
  */
-public class Player {
+public class Player implements Serializable {
+    
+   // class instance variables
+    
+    private String name;
+
+    public Player() {
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + '}';
+    }
+
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
     
 }
