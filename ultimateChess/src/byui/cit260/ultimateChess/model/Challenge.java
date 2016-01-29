ed3810043dc8a96a03cf50ea028583018ff1f8e9
Tanman <Tanman@10.18.151.254>
2016-01-29 09:41:44 -0700

    1/29/2016 These are the classes I will be working on.

---
+++ ultimateChess/src/byui/cit260/ultimateChess/model/Challenge.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.ultimateChess.model;
+
+import java.io.Serializable ;
+
+/**
+ *
+ * @author Tanman
+ */
+public class Challenge implements Serializable{
+    
+    private String description;
+    private int challengeNum;
+    private int reward;
+    
+}
