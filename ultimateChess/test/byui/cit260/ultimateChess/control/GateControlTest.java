7b9032c60c8209805b8b21d4e2396b4bb927126a
Tanman <Tanman@10.18.151.254>
2016-02-12 14:56:42 -0700

    2/12/2016 Moving Along.

--- ultimateChess/test/byui/cit260/ultimateChess/control/GateControlTest.java
+++ ultimateChess/test/byui/cit260/ultimateChess/control/GateControlTest.java
@@ -108,4 +108,76 @@ public class GateControlTest {
         assertEquals(expResult8, result8, 0.0001);
     }
     
+     /**
+     * Test of gateToOpen method, of class GateControl.
+     */
+    @Test
+    public void gateToOpen() {
+        System.out.println("gateToOpen");
+        System.out.println("testcase1");
+        double resistance = 30.0;
+        double current = 20.0;
+        GateControl instance = new GateControl();
+        double expResult = 600.0;
+        double result = instance.gateToOpen(resistance, current);
+        assertEquals(expResult, result,.0001);
+        
+        System.out.println("testcase2");
+        double resistance2 = 321.0;
+        double current2 = -87.0;
+        GateControl instance2 = new GateControl();
+        double expResult2 = -4.0;
+        double result2 = instance2.gateToOpen(resistance2, current2);
+        assertEquals(expResult2, result2, 0.0001);
+       
+         System.out.println("testcase3");
+        double resistance3 = 55;
+        double current3 = 0;
+        GateControl instance3 = new GateControl();
+        double expResult3 = -4;
+        double result3 = instance3.gateToOpen(resistance3, current3);
+        assertEquals(expResult3, result3, 0.0001);
+        
+         System.out.println("testcase4");
+        double resistance4 = 25678.0;
+        double current4 = 3.0;
+        GateControl instance4 = new GateControl();
+        double expResult4 = -3;
+        double result4 = instance4.gateToOpen(resistance4, current4);
+        assertEquals(expResult4, result4, 0.0001);
+        
+         System.out.println("testcase5");
+        double resistance5 = 3.0;
+        double current5 = 53464.0;
+        GateControl instance5 = new GateControl();
+        double expResult5 = -4.0;
+        double result5 = instance5.gateToOpen(resistance5, current5);
+        assertEquals(expResult5, result5, 0.0001);
+        
+         System.out.println("testcase6");
+        double resistance6 = 1000.0;
+        double current6 = 1.0;
+        GateControl instance6 = new GateControl();
+        double expResult6 = 1000.0;
+        double result6 = instance6.gateToOpen(resistance6, current6);
+        assertEquals(expResult6, result6, 0.0001);
+        
+         System.out.println("testcase7");
+        double resistance7 = 1.0;
+        double current7 = 1000.0;
+        GateControl instance7 = new GateControl();
+        double expResult7 = 1000.0;
+        double result7 = instance2.gateToOpen(resistance7, current7);
+        assertEquals(expResult7, result7, 0.0001);
+        
+         System.out.println("testcase8");
+        double resistance8 = 999.0;
+        double current8 = 1.0;
+        GateControl instance8 = new GateControl();
+        double expResult8 = 999.0;
+        double result8 = instance8.gateToOpen(resistance8, current8);
+        assertEquals(expResult8, result8, 0.0001);
+    }
+
+    
 }
