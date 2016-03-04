219ce22c1e9e1a79602cbdd3e9b2e4cb1ae3abb6
Tanman <Tanman@10.18.151.254>
2016-03-04 15:14:43 -0700

    3/4/2016 fixed codeToBreakView/ Time 3:14 pm

--- ultimateChess/src/byui/cit260/ultimateChess/view/GateControlOneView.java
+++ ultimateChess/src/byui/cit260/ultimateChess/view/GateControlOneView.java
@@ -12,90 +12,28 @@ import java.util.Scanner;
  *
  * @author Tanman
  */
-public class GateControlOneView {
-   
-     private String promptMessage;
-     private String promptMessageTwo;     
+public class GateControlOneView extends View{
+       
   
    public GateControlOneView()
      {
-         
+         super("If you know what the resistance and current"
+                 + "\nyou can solve for the voltage. Knowing the voltage will "
+                 + "\nallow you to open this gate\n"
+                 + "\n\nPlease enter the resistance: ");
      }
 
-    public void gateControlOneView()
-     {
-          this.promptMessage = "Please enter the resistance: ";
-          this.promptMessageTwo = "Please enter the current: ";
+   @Override
+    public boolean doAction(String value) {
+       
+        double resistance = Double.parseDouble(value);
         
-        this.displayBanner();
-     
-    }  
-     public void displayBanner(){
-        System.out.println("If you know what the resistance and current"
-                + "\nyou can solve for the voltage. Knowing the voltage will "
-                + "\nallow you to open this gate\n");
-     }
-     
-      public void displayGateControlOneView() {
-       
-        boolean done = false;
-        do {
-            Double resistance = this.getResistance();
-            Double current = this.getCurrent();
-            if (resistance < 1 || resistance  > 1000)
-                return;
-          
-            else if (current < 1 || current > 1000)
-                return;
-            
-            done = this.doAction(resistance, current);
-        } while (!done);
-    }
-         private Double getResistance() {
-         Scanner keyboard = new Scanner(System.in);
-         Double value = -1.0;
-         boolean valid = false;
-         
-         while(!valid){
-             System.out.println("\n" + this.promptMessage);
-             
-             value = keyboard.nextDouble();
-             
-              if (value < 1 || value > 1000) {
-                System.out.println("\nInvalid value: Less than one; Greater "
-                        + "\n than a thousand.");
-                 continue;
-        }
-             break;
-         }
-         
-         return value;
-    }
-         
-          private Double getCurrent() {
-         Scanner keyboard = new Scanner(System.in);
-         Double value = -1.0;
-         boolean valid = false;
-         
-         while(!valid){
-             System.out.println("\n" + this.promptMessageTwo);
-             
-             value = keyboard.nextDouble();
-             
-              if (value < 1 || value > 1000) {
-                System.out.println("\nInvalid value: Less than one; Greater "
-                        + "\n than a thousand.");
-                 continue;
-        }
-             break;
-         }
-         
-         return value;
-    }
-
-    private boolean doAction(Double resistance, Double current) {
-       
-         double voltage = GateControl.gateToOpen(resistance, current);
+        System.out.println("What is the current? ");
+        
+        String value2 = this.getInput();
+        double current = Double.parseDouble(value2);
+        
+        double voltage = GateControl.gateToOpen(resistance, current);
         
         if (voltage != 100){
             System.out.println("\nBetter Luck next time. This isn't the"
@@ -103,16 +41,17 @@ public class GateControlOneView {
             return false;
         }
         //System.out.println(playersName);
-        
+
         // display next view
         this.displayNextView(voltage);
         return true;
-        }
-     private void displayNextView(Double voltage) {
+    }
+
+    private void displayNextView(Double voltage) {
         System.out.println("\n===================================="
-                          + "\n Eureka! " + voltage 
-                          + "\n is in fact the right voltage to"
-                          + "\n open the gate"
-                          + "\n=================================="); 
-     }
+                + "\n Eureka! " + voltage
+                + "\n is in fact the right voltage to"
+                + "\n open the gate"
+                + "\n==================================");
+    }
 }
