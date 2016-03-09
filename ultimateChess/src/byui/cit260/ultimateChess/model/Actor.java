ab706161bbe43e928a05ed3a2f6e73a1a664f65a
Tanman <Tanman@10.18.151.254>
2016-03-09 09:57:43 -0700

    3/9/2016  Time 9:57 AM

--- ultimateChess/src/byui/cit260/ultimateChess/model/Actor.java
+++ ultimateChess/src/byui/cit260/ultimateChess/model/Actor.java
@@ -5,84 +5,31 @@
  */
 package byui.cit260.ultimateChess.model;
 
+import java.awt.Point;
 import java.io.Serializable;
-import java.util.Objects;
 /**
  *
  * @author Taylor
  */
-public class Actor implements Serializable {
+public enum Actor implements Serializable {
     
-    private String name;
-    private String description;
-    private String coordinates;
+   Tanner("Tanman has red hair"),
+    Taylor("Taylor has dark hair");
 
-    public Actor() {
-    }
+    private final String description;
+    private final Point coordinates;
 
-    
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    Actor(String description) {
+    this.description = description;
+    coordinates = new Point(1,1);
+}   
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCoordinates() {
+    public Point getCoordinates() {
         return coordinates;
     }
-
-    public void setCoordinates(String coordinates) {
-        this.coordinates = coordinates;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 79 * hash + Objects.hashCode(this.name);
-        hash = 79 * hash + Objects.hashCode(this.description);
-        hash = 79 * hash + Objects.hashCode(this.coordinates);
-        return hash;
-    }
-
-    @Override
-    public String toString() {
-        return "Actor{" + "name=" + name + ", description=" + description + ", coordinates=" + coordinates + '}';
-    }
-
-    
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Actor other = (Actor) obj;
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        if (!Objects.equals(this.description, other.description)) {
-            return false;
-        }
-        if (!Objects.equals(this.coordinates, other.coordinates)) {
-            return false;
-        }
-        return true;
-    }
-    
     
 }
