22ac9fbeaf55e0012c8c5c00d2b80843804f01cd
Tanman <Tanman@10.18.151.254>
2016-03-18 09:23:52 -0600

    3/18/2016  Time 5:46PM

---
+++ ultimateChess/src/byui/cit260/ultimateChess/model/SceneType.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.ultimateChess.model;
+
+/**
+ *
+ * @author Tanman
+ */
+class SceneType {
+
+    static Object values() {
+        System.out.println("\nI think this should hold the value "
+                + "that specifies a certain scene type");
+        
+        return null;
+    }
+
+    static class start {
+
+        static int ordinal() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        public start() {
+        }
+    }
+
+    static class finish {
+
+        static int ordinal() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        public finish() {
+        }
+    }
+    
+}
